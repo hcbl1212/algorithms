10d6abc0454dd64a76455198ead49a7f38f6c840
Brian Ludwig <bsludwig@svsu.edu>
2019-03-03 19:40:07 -0800

    finished binary search

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,4 @@ FROM ruby:latest
 ENV INSTALL_PATH /algorithms
 RUN mkdir $INSTALL_PATH
 WORKDIR $INSTALL_PATH
-COPY searches/ .
 COPY . .
--- searches/binary_search.rb
+++ searches/binary_search.rb
@@ -1,20 +1,20 @@
 class BinarySearch
-
     def find(ordered_list, look_up)
-        position = nil
-        ordered_list_length = ordered_list.length 
-        look_up_position = (ordered_list_length/2).floor
-        while position.nil? || look_up_position == 0 || look_up_position == ordered_list_length -1 do
-            item_to_compare = ordered_list[look_up_position]
-            if item_to_compare == look_up
-                position = look_up_position
+        low = 0
+        high = ordered_list.length - 1
+        return_value = nil
+        while low <= high do
+            position = ((high + low) / 2).floor
+            item_to_compare = ordered_list[position]
+           if item_to_compare == look_up
+               return_value = position
+               break 
             elsif item_to_compare > look_up
-                look_up_position = ((ordered_list_length - look_up_position)/2).floor
-            elsif item_to_compare < look_up
-                look_up_position = (look_up_position/2).ceil
+                high = position - 1
+            else
+                low = position + 1
             end
         end
-        look_up_position
+        return_value
     end
-
 end
